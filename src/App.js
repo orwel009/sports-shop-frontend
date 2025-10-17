a896a107104856364d31772edabe15ab1059e3ae
orwel009 <orwelvincent1@gmail.com>
2025-10-17 15:56:48 +0530

    profile added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
