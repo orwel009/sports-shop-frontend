efe4384ac8314027e5cbfce80f1a4fa9c4df2596
orwel009 <orwelvincent1@gmail.com>
2025-10-17 15:55:21 +0530

    profile added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Navbar from './components/Navbar';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="container mt-5">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <Navbar/>
+      <div className="container mt-5">
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/profile' element={<Profile/>} />
+          </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import API from '../services/api';
+
+const Profile = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem('token');
+
+      if (!token) {
+        navigate('/login');
+        return;
+      }
+
+      try {
+        const res = await API.get('/auth/profile', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setUser(res.data);
+      } catch (err) {
+        setError(err.response?.data?.msg || 'Failed to fetch user');
+        // localStorage.removeItem('token');
+        // navigate('/login');
+      }
+    };
+
+    fetchUser();
+  }, [navigate]);
+
+  if (error) return <div className="alert alert-danger">{error}</div>;
+
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        {user ? (
+          <div className="card p-4">
+            <h2 className="card-title">Profile</h2>
+            <p><strong>Name:</strong> {user.name}</p>
+            <p><strong>Email:</strong> {user.email}</p>
+            <p><strong>Role:</strong> {user.role}</p>
+          </div>
+        ) : (
+          <p>Loading user info...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
